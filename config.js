c127585fb1d8339e13d7c6d4f4a159ecaea58c30
DominicDev <s0l0estudi4r@gmail.com>
2025-09-25 19:56:42 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'GALANTIS';
+export let nameSongs = [
+	"RUNAWAY (U & I)", /*1*/
+	"NO MONEY", /*2*/
+	"PEANUT BUTTER JELLY", /*3*/
+	"LOVE ON ME", /*4*/
+	"BONES ", /*5*/
+	"HUNTER", /*6*/
+	"YOU", /*7*/
+	"HOLY WATER", /*8*/
+	"RICH BOY", /*9*/
+	"SPACESHIP (FEAT. UFFIE)", /*10*/
+	"GOLD DUST", /*11*/
+	"FIREBIRD", /*12*/
+	"SMILE", /*13*/
+	"FAITH ", /*14*/
+	"STEEL", /*15*/
+	"DNA", /*16*/
+	"LIGHTER", /*17*/
+	"MAMA LOOK AT ME NOW", /*18*/
+	"DUST", /*19*/
+	"ONE, TWO & 3", /*20*/
+]
